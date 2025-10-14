f1aaa1f0e32aff58ea3777bca3c0de37308d1656
difrlkv <136690298+difrlkv@users.noreply.github.com>
2025-10-14 23:46:49 +0300

    04_win_rate.sql

--- sql/04_win_rate.sql
+++ sql/04_win_rate.sql
@@ -1,3 +1,4 @@
+-- Конверсия в оплату
 SELECT COUNT(*) AS total_leads, COUNT(CASE WHEN status = 'Won' THEN 1 END) AS deals_won,
 ROUND(100 * COUNT(CASE WHEN status = 'Won' THEN 1 END) / COUNT(*), 2) AS win_rate 
 FROM deals 
