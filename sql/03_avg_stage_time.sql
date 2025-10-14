46b2f07934a79cdf00414c3f1bd9880b6a4f5f4a
difrlkv <136690298+difrlkv@users.noreply.github.com>
2025-10-14 23:46:28 +0300

    03_avg_stage_time.sql

--- sql/03_avg_stage_time.sql
+++ sql/03_avg_stage_time.sql
@@ -1,3 +1,4 @@
+-- Среднее время между этапами
 SELECT s1.stage_name AS from_stage, s2.stage_name AS to_stage, AVG(DATE_PART('day', s2.entered_at::timestamp - s1.entered_at::timestamp)) AS avg_date
 FROM stages s1
 JOIN stages s2 ON s1.lead_id = s2.lead_id  
