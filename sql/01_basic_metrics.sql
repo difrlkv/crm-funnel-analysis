8bf458d14deb0eb12ea32b09cfb6e46703e881fc
difrlkv <136690298+difrlkv@users.noreply.github.com>
2025-10-14 23:44:48 +0300

    01_basic_metrics.sql

--- sql/01_basic_metrics.sql
+++ sql/01_basic_metrics.sql
@@ -1,13 +1,16 @@
+-- Количество лидов по источникам
 SELECT source, COUNT(*) AS leads_count 
 FROM leads
 GROUP BY 1
 ORDER BY 2 DESC
-
+  
+-- Количество лидов по менеджерам
 SELECT manager_id, COUNT(*) AS leads_count
 FROM leads
 GROUP BY 1
 ORDER BY 2 DESC
-
+  
+-- Количество лидов на каждом этапе воронки
 SELECT stage_name, COUNT(DISTINCT lead_id) AS leads_at_stage
 FROM stages
 GROUP BY 1
