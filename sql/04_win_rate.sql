545ffe2a96eb170281c305b7541e725700d36300
difrlkv <136690298+difrlkv@users.noreply.github.com>
2025-10-14 23:42:56 +0300

    Create 04_win_rate.sql

---
+++ sql/04_win_rate.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(*) AS total_leads, COUNT(CASE WHEN status = 'Won' THEN 1 END) AS deals_won,
+ROUND(100 * COUNT(CASE WHEN status = 'Won' THEN 1 END) / COUNT(*), 2) AS win_rate 
+FROM deals 
